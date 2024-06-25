d7548842db1911fce2fe9adca620fa1d1f6a5d8b
Rob Peck <rob.peck@gmail.com>
2024-06-25 08:07:36 +0100

    feat: sbrp keymap

---
+++ keyboards/keychron/k8_pro/iso/rgb/keymaps/sbrp/config.h
@@ -0,0 +1,8 @@
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+//ignores the layer toggle if you press another key within TAPPING_TERM
+#define IGNORE_MOD_TAP_INTERRUPT
---
+++ keyboards/keychron/k8_pro/iso/rgb/keymaps/sbrp/rules.mk
@@ -0,0 +1,5 @@
+# Disable optimisations for debugging purposes
+LTO_ENABLE = no
+OPT = g
+DEBUG = 3
+SEND_STRING_ENABLE = yes
